4df5d3495498c6b52828670f2aa3741c0c6f6727
quixote15 <santos2tiago@gmail.com>
2021-02-27 18:25:36 -0300

    feature: finish apm tutorial aplication

--- module3/demo-apm/agent/agent.test.js
+++ module3/demo-apm/agent/agent.test.js
@@ -1,11 +1,14 @@
+
 import debug from 'debug'
 const log = debug('agent:test')
 
+import { readdir } from 'fs/promises'
+import { resolve } from 'path'
 import { Server } from 'http'
 import assert from 'assert'
 import { start as InjectMiddleware } from './agent.js'
-const tracker = new assert.CallTracker();
 
+const tracker = new assert.CallTracker();
 
 const eventName = 'request'
 
@@ -15,7 +18,6 @@ const database = [{
     "paidIn": "USD",
     "speaks": ["fr", "pt-br"]
 }]
-
 const user = database[0]
 const request = {
     headers: {
@@ -41,5 +43,21 @@ const serverInstance = new Server()
     assert.deepEqual(request.user.name, user.name)
 }
 
+{
+    const reportsFolder = `${resolve()}/reports`
+    const dirBefore = await readdir(reportsFolder)
+
+    const { headers, user, ...requestData } = request
+    const messageError = "Cannot read property 'x-app-id' of undefined"
+
+    process.on('uncaughtException', async (err) => {
+        if (!(!!~err.message.indexOf(messageError)))
+            return log(err);
+
+        const dirAfter = await readdir(reportsFolder) 
+        assert.notEqual(dirBefore.length, dirAfter.length) 
+    });
+    serverInstance.emit(eventName, requestData, response)
+}
 
 process.on('exit', () => tracker.verify());
---
+++ module3/demo-apm/api/index.js
@@ -0,0 +1,57 @@
+
+import debug from 'debug'
+const log = debug('app:api')
+import Http from 'http'
+
+import database from '../database.json';
+
+import { start } from '../agent/agent.js'
+start(database)
+
+import { pipeline, Transform } from 'stream'
+import { promisify } from 'util'
+
+const pipelineAsync = promisify(pipeline)
+
+const mapData = (lang) => new Transform({
+    destroy(error) {
+        log('called autodestroy!!')
+    },
+    transform: (chunk, encoding, cb) => {
+        const data = JSON.parse(chunk)
+
+        const languageNames = new Intl.DisplayNames([lang], { type: 'currency' });
+
+        const result = JSON.stringify({
+            name: data.name,
+            currency: languageNames.of(data.currency),
+            preferences: data.preferences?.description ?? 'not found'
+        })
+
+        cb(null, result)
+    }
+})
+
+const startServer = async (req, res) => {
+    if (req.method !== 'POST') 
+        return res.end('Hey dude!')
+    
+    try {
+        await pipelineAsync(
+            req,
+            mapData(req.user.speaks),
+            res
+        )
+    } catch (error) {
+        log('Internal server error!', error.message)
+        return res.end('Internal server error')
+    }
+}
+const port = 3000
+Http
+    .createServer(startServer)
+    .listen(port, () => console.log('running! at', port))
+
+export default Http
+// curl -i -H "x-app-id: 1" -X POST -d '{"name":"ErickWendel","currency":"BRL","preferences":{"description":"movies"}}' http://localhost:3000
+// curl -i -H "x-app-id: 2" -X POST -d '{"name":"JsonBecker","currency":"CAD","preferences":null}' http://localhost:3000
