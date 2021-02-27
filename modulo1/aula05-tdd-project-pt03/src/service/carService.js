4047a54d9ee342560d4c276e8ec9751bdd33970b
quixote15 <santos2tiago@gmail.com>
2021-02-27 16:18:56 -0300

    refact modules

---
+++ modulo1/aula05-tdd-project-pt03/src/service/carService.js
@@ -0,0 +1,32 @@
+const BaseRepository = require('../entities/repository/base/baseRepository');
+
+class CarService {
+  constructor({cars}) {
+    this.carRepository = new BaseRepository({file: cars});
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    });
+  }
+
+  getRandomPositionFromArray(list) {
+    const listLength = list.length;
+    return Math.floor(
+      Math.random() * listLength
+    )
+  }
+
+  chooseRandomCar(carCategory){
+    const position = this.getRandomPositionFromArray(carCategory.carIds);
+    const car = carCategory.carIds[position];
+    return car;
+  }
+
+  getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    const car = this.carRepository.find(carId);
+    return car;
+  }
+}
+
+module.exports = CarService;
